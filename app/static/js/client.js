26370badfd892b32ca73756345270b7347018472
Dain Saint <dain@cipherprime.com>
2024-11-25 09:46:51 -0500

    remove websocket logic in prep for SSE

--- app/static/js/client.js
+++ app/static/js/client.js
@@ -1,23 +1,7 @@
-import Socket from "./socket.js";
-
-let socket;
-
 const init = () => {
-  // socket = new Socket("ws://localhost:3000");
-  // socket.onData = onData;
-  // socket.connect();
-
-  const button = document.querySelector("#send");
-  button?.addEventListener("click", () => {
-    socket.sendData({
-      action: "new-game"
-    })
-  })
-
   window.addEventListener("hashchange", updateHashes);
   updateHashes();
 }
-
 
 const updateHashes = () => {
   for( const anchor of document.querySelectorAll("a[href^='#']") ) {
@@ -25,19 +9,5 @@ const updateHashes = () => {
     anchor.classList.toggle("active", isActive);
   }
 }
-
-
-const onData = (data) => {
-  data.games.forEach( game => {
-    const toRender = `
-      <li data-id="${game._id}">
-        ${ game.date.toDateString() } [${game.sessions.length} session(s)]
-      </li>
-    `;
-    
-    document.querySelector("#games")?.append( toRender );
-  })
-}
-
 
 document.addEventListener("DOMContentLoaded", init);
