625c399a13b0ac49e28d1200248652a76c262b0b
Dain Saint <dain@cipherprime.com>
2025-03-26 18:07:17 -0400

    and ticking works now

--- app/routes/json.js
+++ app/routes/json.js
@@ -1,50 +1,50 @@
-// import Ledger from "#database/ledger";
+import Ledger from "#database/ledger";
 import Episode from "#models/episode";
 import express from "express";
 
 
 const jsonRouter = express.Router();
 
-// jsonRouter.use((req, res, next) => {
-//   res.type("json");
-//   next();
-// })
+jsonRouter.use((req, res, next) => {
+  res.type("json");
+  next();
+})
 
-// jsonRouter.get("/games", (req, res) => {
-//   res.send(Ledger.games);
-// });
+jsonRouter.get("/games", (req, res) => {
+  res.send(Ledger.games);
+});
 
-// jsonRouter.get("/episodes", (req, res) => {
-//   res.send(Ledger.episodes);
-// });
+jsonRouter.get("/episodes", (req, res) => {
+  res.send(Ledger.episodes);
+});
 
-// jsonRouter.get("/episodes/:episodeId", (req, res) => {
-//   const { episodeId } = req.params;
-//   const episode = Episode.load(episodeId)
-//   res.send(episode);
-// });
+jsonRouter.get("/episodes/:episodeId", (req, res) => {
+  const { episodeId } = req.params;
+  const episode = Episode.load(episodeId)
+  res.send(episode);
+});
 
-// //TODO: These requests should be scoped to either a specific society, or the active society (if one exists)
-// jsonRouter.get("/communities", (req, res) => {
-//   const activeEpisode = Ledger.getActiveEpisode();
-//   const communities = activeEpisode?.communities || [];
-//   res.send(communities);
-// });
+//TODO: These requests should be scoped to either a specific society, or the active society (if one exists)
+jsonRouter.get("/communities", (req, res) => {
+  const activeEpisode = Ledger.getActiveEpisode();
+  const communities = activeEpisode?.communities || [];
+  res.send(communities);
+});
 
-// jsonRouter.get("/players", (req, res) => {
-//   const activeEpisode = Ledger.getActiveEpisode();
-//   const players = activeEpisode?.players || [];
-//   res.send(players);
-// });
+jsonRouter.get("/players", (req, res) => {
+  const activeEpisode = Ledger.getActiveEpisode();
+  const players = activeEpisode?.players || [];
+  res.send(players);
+});
 
-// jsonRouter.get("/societies", (req, res) => {
-//   const activeEpisode = Ledger.getActiveEpisode();
-//   const societies = activeEpisode?.societies || [];
-//   res.send(societies);
-// });
+jsonRouter.get("/societies", (req, res) => {
+  const activeEpisode = Ledger.getActiveEpisode();
+  const societies = activeEpisode?.societies || [];
+  res.send(societies);
+});
 
-// jsonRouter.use((err, req, res, next)=> {
-//   res.status(err.status || 400).json()
-// })
+jsonRouter.use((err, req, res, next)=> {
+  res.status(err.status || 400).json()
+})
 
 export default jsonRouter;
