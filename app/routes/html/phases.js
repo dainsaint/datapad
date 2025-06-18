8e2de5cff5127e2bff297d4c87b3501052792e54
Dain Saint <dain@cipherprime.com>
2025-06-18 18:43:53 -0400

    phase sync

--- app/routes/html/phases.js
+++ app/routes/html/phases.js
@@ -55,7 +55,7 @@ phases.post("/:episodeId/round", async (req, res, next) => {
 
 phases.get("/:episodeId/:phaseId/:view?", async (req, res, next) => {
   const { episodeId, phaseId, view = "card" } = req.params;
-
+console.log( episodeId, phaseId );
   const episode = await Episode.load(episodeId);
   const phase = episode.getPhaseById(phaseId);
 
